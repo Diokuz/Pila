066f3ada4e0cd3cbf79962bb94817a5f2c692dc0
Diokuz <diokuz@gmail.com>
2014-05-29 20:32:54 -0700

    Update pila.js

--- bin/pila.js
+++ bin/pila.js
@@ -3,8 +3,6 @@ var fs = require('fs');
 var pila = require('../lib/pila');
 var argv = require('yargs').argv;
 
-var files = process.argv.slice(1);
-
 argv['_'].forEach(function(filepath) {
     var contents = fs.readFileSync(filepath).toString();
     var processed = pila(contents);
